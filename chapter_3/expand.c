d4488e474220848dfa56a773de10172c6f3a96bb
cshemadt <adustoevsky@gmail.com>
2022-06-25 17:34:17 +0300

    Test case with the same characters covered

--- chapter_3/expand.c
+++ chapter_3/expand.c
@@ -3,7 +3,7 @@
 #define MAX_LENGTH 1000
 void expand(char s1[], char s2[]);
 int main() {
-    char s1[] = "a-z0-9b-d";
+    char s1[] = "--a--a-z";
     char s2[MAX_LENGTH]="";
     expand(s1,s2);
     printf("%s\n",s2);
@@ -13,7 +13,11 @@ void expand(char s1[], char s2[]) {
     char from,to;
     for(i=1,j=0;i<strlen(s1)-1;i++) {
         if(s1[i] == '-' && (s1[i-1]!='-' && s1[i+1]!='-')) {
+			int prev=to;
             from = s1[i-1];
+			if(prev == from) {
+				from++;
+			}
             to = s1[i+1];
             for(;from<=to;from++,j++) {
                 s2[j]=from;
