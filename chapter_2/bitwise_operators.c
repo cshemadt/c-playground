7de5d61ee7e45e34dd2b3b23980e30287d8f6e9a
cshemadt <adustoevsky@gmail.com>
2022-06-25 18:44:56 +0300

    Bitwise operators

--- chapter_2/bitwise_operators.c
+++ chapter_2/bitwise_operators.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-
-int main(void) {
-    printf("");
+#include <math.h>
+#define MAX_LENGTH 1000
+void bin(char binary[],int n) {
+	long i;
+	int ind=0;
+	for(i=1<<8;i>0;i/=2) {
+		if((n&i)==0)
+			binary[ind++]='0';
+		else
+			binary[ind++]='1';
+	}
+	binary[ind]='\0';
+}
+// 2-6
+int setbits(int x,int n);
+int main() {
+	int x=180;
+	printf("%d\n",setbits(x, 3));
+}
+int setbits(int x,int n) {
+	int mask=pow(2,n)-1;
+	return x | mask;
 }
