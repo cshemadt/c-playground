925a588d511b6fc982d182df8a5f7834ca9e7e70
cshemadt <adustoevsky@gmail.com>
2022-06-25 13:11:14 +0300

    Binary search

--- chapter_3/binary_search.c
+++ chapter_3/binary_search.c
@@ -2,10 +2,6 @@
 int iteration_binary_search(int x, int v[], int n);
 int recursive_binary_search(int x,int low,int hight, int v[]);
 int main() {
-    int n=9;
-    int v[]={1,2,3,5,7,8,12,19,100};   
-    int ind=recursive_binary_search(1,0,n-1,v);
-    printf("%d\n",ind);
 }
 int iteration_binary_search(int x, int v[], int n) {
     int low,mid,high;
